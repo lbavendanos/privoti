6497eedf59b0dcfb938c840e412ca667d63d068f
lbavendanos <lbavendanos@gmail.com>
2023-02-23 20:48:51 -0500

    Add tailwindcss

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import './globals.css'
+import '@/styles/app.css'
 
 export default function RootLayout({
   children,
@@ -7,11 +7,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
-      <head />
       <body>{children}</body>
     </html>
   )
