62820041c1226038d7d5ff2955193b0581c9a49d
lbavendanos <lbavendanos@gmail.com>
2023-02-26 17:17:49 -0500

    Add products title

--- src/modules/home/HomeModule.tsx
+++ src/modules/home/HomeModule.tsx
@@ -8,18 +8,23 @@ export default function HomeModule() {
     <div className="flex flex-col">
       <HomeBanner />
       <Container className="my-10">
-        <div
-          className={cn(
-            'grid grid-cols-2 gap-4',
-            'md:grid-cols-3',
-            'lg:grid-cols-4',
-            'xl:grid-cols-5',
-            '2xl:grid-cols-6'
-          )}
-        >
-          {Array.from({ length: 24 }, (_, i) => (
-            <HomeProduct key={i} />
-          ))}
+        <div className="flex flex-col space-y-3">
+          <h2 className="uppercase font-semibold tracking-tight text-xl md:text-2xl">
+            Featured products
+          </h2>
+          <div
+            className={cn(
+              'grid grid-cols-2 gap-4',
+              'md:grid-cols-3',
+              'lg:grid-cols-4',
+              'xl:grid-cols-5',
+              '2xl:grid-cols-6'
+            )}
+          >
+            {Array.from({ length: 24 }, (_, i) => (
+              <HomeProduct key={i} />
+            ))}
+          </div>
         </div>
       </Container>
     </div>
