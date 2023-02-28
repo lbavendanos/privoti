faaa87a752193704d87284d4350ecfb0b7ebc607
lbavendanos <lbavendanos@gmail.com>
2023-02-27 22:28:58 -0500

    Add formcontrol and formlabel

---
+++ src/common/components/FormControl.tsx
@@ -0,0 +1,36 @@
+import { cn } from 'lib/utils/helpers'
+import React from 'react'
+
+export interface FormControlProps extends React.ComponentPropsWithRef<'input'> {
+  multiline?: boolean
+  rows?: number
+}
+
+const FormControl = React.forwardRef<any, any>(
+  ({ className, multiline, ...props }, ref) => {
+    const formControlClassName = [
+      'w-full h-auto p-2.5',
+      'border border-zinc-800 outline-none',
+      'text-xs text-zinc-500 tracking-tight',
+      'placeholder:uppercase placeholder:tracking-tight',
+    ]
+
+    return multiline ? (
+      <textarea
+        {...props}
+        ref={ref}
+        className={cn(formControlClassName, className)}
+      />
+    ) : (
+      <input
+        {...props}
+        ref={ref}
+        className={cn(formControlClassName, className)}
+      />
+    )
+  }
+)
+
+FormControl.displayName = 'FormControl'
+
+export default FormControl
---
+++ src/common/components/FormLabel.tsx
@@ -0,0 +1,24 @@
+import { cn } from 'lib/utils/helpers'
+import React from 'react'
+
+export interface FormLabelProps extends React.ComponentPropsWithRef<'label'> {}
+
+const FormLabel = React.forwardRef<HTMLLabelElement, FormLabelProps>(
+  ({ className, ...props }, ref) => {
+    const formLabelClassName = [
+      'uppercase font-light text-sm text-zinc-800 tracking-tight',
+    ]
+
+    return (
+      <label
+        {...props}
+        ref={ref}
+        className={cn(formLabelClassName, className)}
+      />
+    )
+  }
+)
+
+FormLabel.displayName = 'FormLabel'
+
+export default FormLabel
