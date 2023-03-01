ad37e5f4570b5e5a6fa282220821078a444a5ad3
lbavendanos <lbavendanos@gmail.com>
2023-03-01 10:22:56 -0500

    Update logo

--- src/layouts/base/components/BaseLogo.tsx
+++ src/layouts/base/components/BaseLogo.tsx
@@ -1,9 +1,11 @@
-import { cn } from 'lib/utils/helpers'
+import { cn, config } from 'lib/utils/helpers'
 import Link from 'next/link'
 
 interface BaseLogoProps extends React.ComponentPropsWithoutRef<'a'> {}
 
 export default function BaseLogo({ className, ...props }: BaseLogoProps) {
+  const appName = config<string>('app.name')
+
   return (
     <Link
       {...props}
@@ -14,9 +16,7 @@ export default function BaseLogo({ className, ...props }: BaseLogoProps) {
         className
       )}
     >
-      <span className="text-primary-400">PRI</span>
-      <span className="text-secondary-400">VO</span>
-      <span className="text-tertiary-400">TI</span>
+      {appName}
     </Link>
   )
 }
