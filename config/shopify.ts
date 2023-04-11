48b2a7455be6c1f13f1b1729d87bd957cea4ae3d
lbavendanos <lbavendanos@gmail.com>
2023-04-11 12:51:47 -0500

    Update shopify version

--- config/shopify.ts
+++ config/shopify.ts
@@ -2,7 +2,7 @@ const shopify = {
   domain:
     process.env.NEXT_PUBLIC_SHOPIFY_STORE_DOMAIN || 'your-store.myshopify.com',
   api_version:
-    process.env.NEXT_PUBLIC_SHOPIFY_STOREFRONT_API_VERSION || '2023-01',
+    process.env.NEXT_PUBLIC_SHOPIFY_STOREFRONT_API_VERSION || '2023-04',
   public_token:
     process.env.NEXT_PUBLIC_SHOPIFY_STOREFRONT_PUBLIC_ACCESS_TOKEN || '',
   private_token: process.env.SHOPIFY_STOREFRONT_PRIVATE_ACCESS_TOKEN || '',
